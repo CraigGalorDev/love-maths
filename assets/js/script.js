ffe19ceda76d9f0ba70246fa578029cc71277b67
CraigGalorDev <craigmitchellasus@gmail.com>
2022-03-28 18:02:34 +0000

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,9 @@ function runGame(gameType){
     else if(gameType === "multiply"){
         displayMultiplyQuestion(num1 , num2);
     }
+    else if(gameType === "subtract"){
+        displaySubtractQuestion(num1 , num2);
+    }
     else{
         alert( `Unkown game type: + ${gameType}`)
         throw `Unkown game type: + ${gameType}. Aborting!`
@@ -72,6 +75,9 @@ function calculateCorrectAnswer(){
     else if(operator === "x"){
         return[operand1 * operand2 , "multiply"];
     }
+    else if(operator === "-"){
+        return[operand1 - operand2 , "subtract"];
+    }
     else{
         alert(`Unimplemented opertator ${operator}`);
         throw `Unimplemented opertator ${operator} . Aborting!`;
@@ -97,7 +103,11 @@ function displayAdditionQuestion(operand1,operand2){
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
 }  
-function displaySubtractQuestion(){} 
+function displaySubtractQuestion(operand1,operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+} 
 
 function displayMultiplyQuestion(operand1,operand2){
     document.getElementById('operand1').textContent = operand1;
